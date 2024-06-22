c9a73ffd010829b90bc7eba642a4a3baedb2137d
wissseman777 <161206102+wissseman777@users.noreply.github.com>
2024-06-22 18:22:30 +0200

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,44 +1,45 @@
-    }
-}
-    const data = generateRandomData();
-    const data = [];
-
-    return data;
-    });
-    data.forEach(item => {
-        console.log("Random Number: " + item);
-    for (let i = 0; i < 10; i++) {
-}
-
-function generateRandomData() {
-function main() {
-        data.push(Math.floor(Math.random() * 100) + 1);
-
-main();function generateRandomData() {
-function main() {
 main();
 
-    });
         data.push(Math.floor(Math.random() * 100) + 1);
-
-    return data;
-        console.log("Random Number: " + item);
-    for (let i = 0; i < 10; i++) {
-}
-    }
-    const data = generateRandomData();
-}
+function main() {
+function generateRandomData() {
+    });
     data.forEach(item => {
 
-    const data = [];    public static void main(String[] args) {
-            data[i] = (int) (Math.random() * 100) + 1;
-        for (int i = 0; i < 10; i++) {
-        for (int item : data) {
-    }
-        }
-            System.out.println("Random Number: " + item);
-public class RandomDataGenerator {
-        int[] data = new int[10];
-        }
+    return data;
+    const data = generateRandomData();
+    for (let i = 0; i < 10; i++) {
+}
+        console.log("Random Number: " + item);
 
 }
+    }
+    const data = [];        console.log("Random Number: " + item);
+    for (let i = 0; i < 10; i++) {
+
+    const data = generateRandomData();
+        data.push(Math.floor(Math.random() * 100) + 1);
+    });
+function main() {
+    const data = [];
+}
+function generateRandomData() {
+    data.forEach(item => {
+    }
+
+    return data;
+}
+main();
+        for (int i = 0; i < 10; i++) {
+
+}
+    }
+
+        int[] data = new int[10];
+            data[i] = (int) (Math.random() * 100) + 1;
+public class RandomDataGenerator {
+        }
+        }
+    public static void main(String[] args) {
+            System.out.println("Random Number: " + item);
+        for (int item : data) {
